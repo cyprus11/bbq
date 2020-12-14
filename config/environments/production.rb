3d76ae3a2d54cd282998aa32a6b5c145029ec2ed
Alexey <alexx10@mail.ru>
2020-12-14 07:33:22 +0300

    change action mailer settings for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,15 +78,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'still-cliffs-65918.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.ru',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
 
