eda04e1adb1af030aecc800f9d0c297812dcfde4
Alexey <alexx10@mail.ru>
2020-12-14 22:45:36 +0300

    add default value for action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# default template for action mailer
+ActionMailer::Base.default :content_type => "text/html"
