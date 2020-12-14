017e0dc9f45a225208a589785cc7b3b3d885fbd0
Alexey <alexx10@mail.ru>
2020-12-14 22:52:24 +0300

    other test

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# default template for action mailer
-ActionMailer::Base.default :content_type => "text/html"
