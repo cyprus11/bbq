e78c81b09061889297ca651443c53d22f43e0e1d
Alexey <alexx10@mail.ru>
2020-12-15 22:37:51 +0300

    change mailjet_api on mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'https://still-cliffs-65918.herokuapp.com/', port: 443 }
-  config.action_mailer.delivery_method = :mailjet_api
+  config.action_mailer.delivery_method = :mailjet
   # settings for mailjet
   # ActionMailer::Base.smtp_settings = {
   #   :port           => ENV['MAILJET_SMTP_PORT'],
