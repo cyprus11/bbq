95bfbde941a612f89a73f44db1e108550f013d8d
Alexey <alexx10@mail.ru>
2020-12-16 21:49:05 +0300

    add localozation for mail templates and remove some empty lines

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,24 +1,23 @@
 class EventMailer < ApplicationMailer
-
   def subscription(event, subscription)
     @email = subscription.user_email
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: event.user.email, subject: t('.subject', title: @event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: t('.subject', title: @event.title)
   end
 
   def photo(event, photo, email)
     @event = event
     @photo = photo
 
-    mail to: email, subject: "Новая фотография @ #{event.title}"
+    mail to: email, subject: t('.subject', title: @event.title)
   end
 end
