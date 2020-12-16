250596ca45162ac8bb0625d664b01f451b5e509b
Alexey <alexx10@mail.ru>
2020-12-16 16:29:19 +0300

    add helper current_user_can_load_photo?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user_can_edit?
+  helper_method :current_user_can_load_photo?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
+  end
+
+  def current_user_can_load_photo?(event)
+    user_signed_in? && event.subscriptions.include?(current_user)
   end
 
   def current_user_can_edit?(model)
