1ea7f0a77398b74e2a4ed83b84d32582c607cfd3
Alexey <alexx10@mail.ru>
2020-12-18 20:22:37 +0300

    add times symbol on flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
-        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat content_tag(:button, '×', class: "close", data: { dismiss: 'alert' })
         concat message
       end)
     end
