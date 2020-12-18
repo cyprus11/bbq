8b38da0f1f201e8d964ff3aa599e82e19b723341
Alexey <alexx10@mail.ru>
2020-12-18 23:04:47 +0300

    add ekko-lightbox

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,8 +2,10 @@ require("@rails/ujs").start()
 require('data-confirm-modal')
 import "bootstrap/dist/js/bootstrap"
 import "air-datepicker/dist/js/datepicker.min"
-import "../scripts/custom"
+import "ekko-lightbox/dist/ekko-lightbox.min"
 import "../scripts/map"
+import "../scripts/ekko-lightbox-init"
+import "../scripts/custom"
 import "../styles/application"
 
 const images = require.context('../images', true)
