40eddb0760baa78c085b377ff5c6e1807af9408b
Alexey <alexx10@mail.ru>
2020-12-18 22:12:59 +0300

    add yandex map to events

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,7 @@ require('data-confirm-modal')
 import "bootstrap/dist/js/bootstrap"
 import "air-datepicker/dist/js/datepicker.min"
 import "../scripts/custom"
+import "../scripts/map"
 import "../styles/application"
 
 const images = require.context('../images', true)
