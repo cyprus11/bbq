dd44bb27065dfc6d3699e635195bc8b22c8a325f
Alexey <alexx10@mail.ru>
2021-01-08 12:07:58 +0300

    small fix again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:s3_access_key],
-      aws_access_access_key: Rails.application.credentials.aws[:s3_secret_key],
+      aws_secret_access_key: Rails.application.credentials.aws[:s3_secret_key],
       region: Rails.application.credentials.aws[:s3_region_name]
     }
 
