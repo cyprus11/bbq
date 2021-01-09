e83b8ffd976132ab222d7440cc59a9558926b0eb
Alexey <alexx10@mail.ru>
2021-01-09 15:15:51 +0300

    add validation and js code for upload image

--- app/frontend/scripts/custom.js
+++ app/frontend/scripts/custom.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     $('#event_datetime').datepicker({
         minDate: new Date()
+    });
+    $('#submitButton').attr('disabled',true);
+    $('#photo').change(function() {
+        if ($(this).val().length != 0)
+            $('#submitButton').attr('disabled',false);
+        else
+            $('#submitButton').attr('disabled',true);
     })
 });
