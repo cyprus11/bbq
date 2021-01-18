9375d4faa68047694cdec3c5035f05f7e65b74f1
Alexey <alexx10@mail.ru>
2021-01-18 21:38:57 +0300

    commit after deploy string

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ append :linked_files, "config/database.yml", "config/master.key"
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
-after 'deploy:restart'
+# after 'deploy:restart'
 # after 'deploy:restart', 'resque:restart'
 
 # Default value for default_env is {}
