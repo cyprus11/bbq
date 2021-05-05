f154ee41669660dc369e3fb00ce158a97b3af434
Alexey <alexx10@mail.ru>
2021-05-05 12:12:34 +0300

    Disable raise delivery status and change default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://tav-dev.tk/', port: 80 }
+  config.action_mailer.default_url_options = { host: 'https://bbq.tav-dev.tk', port: 443 }
   config.action_mailer.delivery_method = :mailjet
   # settings for mailjet
   # ActionMailer::Base.smtp_settings = {
